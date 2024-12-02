7a29201fc0e3f63444f3d4c73b62a39310135663
SiongSng <rrt467778@gmail.com>
2024-12-03 00:47:37 +0800

    cd: try deploy to VPS

---
+++ src/endpoints/HealthCheck.ts
@@ -0,0 +1,46 @@
+import { OpenAPIRoute } from "chanfana";
+import { AppContext } from "index";
+import { z } from "zod";
+
+export class HealthCheck extends OpenAPIRoute {
+  schema = {
+    tags: ["Health Check"],
+    summary: "Health Check (Internal)",
+    responses: {
+      "200": {
+        description: "Returns a health check response",
+        content: {
+          "application/json": {
+            schema: z.object({
+              status: z.string(),
+              environment: z.string(),
+              timestamp: z.string().datetime(),
+            }),
+          },
+        },
+      },
+    },
+  };
+
+  async handle(ctx: AppContext) {
+    const db = ctx.var.prisma;
+
+    let status = "ok";
+    try {
+      // Try a simple query to check DB connectivity
+      await db.$queryRaw`SELECT 1`;
+    } catch (err) {
+      console.error(err);
+      status = "error";
+    }
+
+    return ctx.json(
+      {
+        status,
+        environment: process.env.CURRENT_ENVIRONMENT || "unknown",
+        timestamp: new Date().toISOString(),
+      },
+      500,
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import { prismaInitMiddleware } from "config/prisma";
 import dotenv from "dotenv";
 import { logger } from "hono/logger";
 import { PrismaClient } from "@prisma/client";
+import { HealthCheck } from "endpoints/HealthCheck";
+import { getConnInfo } from "hono/bun";
+import { ipRestriction } from "hono/ip-restriction";
 
 interface Variables {
   prisma: PrismaClient;
@@ -20,8 +23,17 @@ const openapi = fromHono(app, {
 });
 openapi.use(logger());
 openapi.use(prismaInitMiddleware);
+
 // Register OpenAPI endpoints
 openapi.get("/resources/partner-school", ListPartnerSchool);
+openapi.use(
+  "/health",
+  ipRestriction(getConnInfo, {
+    denyList: [],
+    allowList: ["127.0.0.1", "::1"],
+  }),
+);
+openapi.get("/health", HealthCheck);
 
 export default {
   port: process.env.PORT || 8787,
