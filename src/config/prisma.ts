839f75d6ebe36b1afa3eabe127412c824bca40d7
SiongSng <rrt467778@gmail.com>
2024-12-08 21:20:42 +0800

    fix: cache database connection

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -3,14 +3,24 @@ import { MiddlewareHandler } from "hono";
 import { AppOptions } from "index";
 import env from "./env";
 
+let prisma: PrismaClient | null = null;
+
+function initPrisma() {
+  const connectionString = env.DATABASE_URL;
+  const client = new PrismaClient({
+    datasources: { db: { url: connectionString } },
+  });
+
+  prisma = client;
+}
+
 export const prismaInitMiddleware: MiddlewareHandler<AppOptions> = async (ctx, next) => {
   if (!ctx.var.prisma) {
-    const connectionString = env.DATABASE_URL;
-    const client = new PrismaClient({
-      datasources: { db: { url: connectionString } },
-    });
+    if (!prisma) {
+      initPrisma();
+    }
 
-    ctx.set("prisma", client as never);
+    ctx.set("prisma", prisma as never);
   }
   await next();
 };
