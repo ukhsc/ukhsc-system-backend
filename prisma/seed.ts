807011ceae6d8f433e0fdaf2d16eaa1ac97181a4
SiongSng <48402225+SiongSng@users.noreply.github.com>
2024-12-14 20:58:03 +0800

    git: rebase to main

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PartnerPlan, PrismaClient } from "@prisma/client";
+import { PartnerSchoolPlan, PrismaClient } from "@prisma/client";
 import console from "console";
 import process from "process";
 
@@ -9,42 +9,42 @@ async function main() {
     {
       short_name: "文山高中",
       full_name: "高雄市立文山高級中學",
-      plan: PartnerPlan.Combined,
+      plan: PartnerSchoolPlan.Combined,
     },
     {
       short_name: "三民高中",
       full_name: "高雄市立三民高級中學",
-      plan: PartnerPlan.Personal,
+      plan: PartnerSchoolPlan.Personal,
     },
     {
       short_name: "左營高中",
       full_name: "高雄市立左營高級中學",
-      plan: PartnerPlan.GroupA,
+      plan: PartnerSchoolPlan.GroupA,
     },
     {
       short_name: "前鎮高中",
       full_name: "高雄市立前鎮高級中學",
-      plan: PartnerPlan.Personal,
+      plan: PartnerSchoolPlan.Personal,
     },
     {
       short_name: "小港高中",
       full_name: "高雄市立小港高級中學",
-      plan: PartnerPlan.Combined,
+      plan: PartnerSchoolPlan.Combined,
     },
     {
       short_name: "仁武高中",
       full_name: "高雄市立仁武高級中學",
-      plan: PartnerPlan.Combined,
+      plan: PartnerSchoolPlan.Combined,
     },
     {
       short_name: "林園高中",
       full_name: "高雄市立林園高級中學",
-      plan: PartnerPlan.Combined,
+      plan: PartnerSchoolPlan.Combined,
     },
     {
       short_name: "高師大附中",
       full_name: "國立高雄師範大學附屬高級中學",
-      plan: PartnerPlan.Personal,
+      plan: PartnerSchoolPlan.Personal,
     },
   ];
 
