dfd8f9fb20a68c534cc81ca225b53275ce3f7b55
SiongSng <rrt467778@gmail.com>
2024-12-14 13:28:39 +0800

    feat: use upsert for database seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,45 +5,63 @@ import process from "process";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.partnerSchool.createMany({
-    data: [
-      {
-        short_name: "文山高中",
-        full_name: "高雄市立文山高級中學",
-        plan: PartnerPlan.Combined,
+  const schools = [
+    {
+      short_name: "文山高中",
+      full_name: "高雄市立文山高級中學",
+      plan: PartnerPlan.Combined,
+    },
+    {
+      short_name: "三民高中",
+      full_name: "高雄市立三民高級中學",
+      plan: PartnerPlan.Personal,
+    },
+    {
+      short_name: "左營高中",
+      full_name: "高雄市立左營高級中學",
+      plan: PartnerPlan.GroupA,
+    },
+    {
+      short_name: "前鎮高中",
+      full_name: "高雄市立前鎮高級中學",
+      plan: PartnerPlan.Personal,
+    },
+    {
+      short_name: "小港高中",
+      full_name: "高雄市立小港高級中學",
+      plan: PartnerPlan.Combined,
+    },
+    {
+      short_name: "仁武高中",
+      full_name: "高雄市立仁武高級中學",
+      plan: PartnerPlan.Combined,
+    },
+    {
+      short_name: "林園高中",
+      full_name: "高雄市立林園高級中學",
+      plan: PartnerPlan.Combined,
+    },
+    {
+      short_name: "高師大附中",
+      full_name: "國立高雄師範大學附屬高級中學",
+      plan: PartnerPlan.Personal,
+    },
+  ];
+
+  for (const school of schools) {
+    await prisma.partnerSchool.upsert({
+      where: {
+        short_name: school.short_name,
+        full_name: school.full_name,
       },
-      {
-        short_name: "三民高中",
-        full_name: "高雄市立三民高級中學",
-        plan: PartnerPlan.Personal,
+      update: {
+        plan: school.plan,
       },
-      {
-        short_name: "左營高中",
-        full_name: "高雄市立左營高級中學",
-        plan: PartnerPlan.GroupA,
+      create: {
+        ...school,
       },
-      {
-        short_name: "前鎮高中",
-        full_name: "高雄市立前鎮高級中學",
-        plan: PartnerPlan.Personal,
-      },
-      {
-        short_name: "小港高中",
-        full_name: "高雄市立小港高級中學",
-        plan: PartnerPlan.Combined,
-      },
-      {
-        short_name: "仁武高中",
-        full_name: "高雄市立仁武高級中學",
-        plan: PartnerPlan.Combined,
-      },
-      {
-        short_name: "林園高中",
-        full_name: "高雄市立林園高級中學",
-        plan: PartnerPlan.Combined,
-      },
-    ],
-  });
+    });
+  }
 }
 
 main()
